c012f778787f73aa022e46e9f105a6bbcadf0db6
Lucia Alday <123031285+luciaalday@users.noreply.github.com>
2025-07-09 13:10:05 -0700

    Update regular_TicTacToe.py
    Shorten from 61 lines to 6 (but they're ugly)

--- regular_TicTacToe.py
+++ regular_TicTacToe.py
@@ -1,61 +1,13 @@
 '''
-    author: Lucia Alday
-    for regular tic-tac-toe
+author: Lucia Alday (with some help from contributors Race Williams and Charlie McLaughlin)
+file: regular_TicTacToe.py
+purpose: make a shorter version than Race (whose min was 12 lines) and demonstrate normal game play of tic tic toe in terminal
 '''
 
-def turns(b):
-    valid = False
-    while valid == False:
-        if b[9] % 2 == 0:
-            xplay = int(input("Enter placement of next X:\n"))
-            if xplay < 9 and b[xplay] == ' ':
-                valid, b[xplay] = True, 'X'
-        else:
-            oplay = int(input("Your turn O:\n"))
-            if oplay < 9 and b[oplay] == ' ':
-                valid, b[oplay] = True, 'O'
-    b[9] += 1
-    current_turn(b)
-    return b
-
-def check_wins(b):
-    if (b[0] == b[4] and b[4] == b[8]) or (b[1] == b[4] and b[4] == b[7]) or (b[2] == b[4] and b[4] == b[6]) or (b[3] == b[4] and b[4] == b[5]):
-        if b[4] == 'X':
-            print("X wins! Womp womp O")
-            return True
-        elif b[4] == 'O':
-            print("O wins! ")
-    elif (b[0] == b[1] and b[1] == b[2]) or (b[0] == b[3] and b[3] == b[6]):
-        if b[0] == 'X':
-            print("X wins!")
-            return True
-        elif b[0] == "O":
-            print("O wins!")
-            return True
-    elif (b[8] == b[7] and b[7] == b[6]) or (b[8] == b[5] and b[8] == b[2]):
-        if b[0] == 'X':
-            print("X wins!")
-            return True
-        elif b[0] == "O":
-            print("O wins!")
-            return True
-    elif ' ' not in b:
-        print("It's a tie! One of you needs to get better at this")
-        return True
-    return False
-
-def current_turn(b):
-    print(' ' + b[0] + ' | ' + b[1] + ' | ' +  b[2] + ' ')
-    print('___|___|___')
-    print(' ' + b[3] + ' | ' + b[4] + ' | ' +  b[5] + ' ')
-    print('___|___|___')
-    print(' ' + b[6] + ' | ' + b[7] + ' | ' +  b[8] + ' ')
-    print('   |   |   ')
-
-def main():
-    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 0]
-    current_turn(['0', '1', '2', '3', '4', '5', '6', '7', '8'])
-    while check_wins(board) != True:
-        board = turns(board)
-
-main()
+b, play, turn = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', 'O'], 9, 0
+print(' 0 | 1 | 2 \n___|___|___\n 3 | 4 | 5 \n___|___|___\n 6 | 7 | 8 \n   |   |   ')
+while turn < 9 and turn >= 0 and not (((b[0]==b[4]==b[8] or b[1]==b[4]==b[7] or b[2]==b[4]==b[6] or b[3]==b[4]==b[5]) and b[4] in 'XO')or ((b[0]==b[1]==b[2] or b[0]==b[3]==b[6]) and b[0]in 'XO')or ((b[8]==b[7]==b[6] or b[5]==b[8]==b[2]) and b[8]in 'XO')):
+    while play > 8 or b[play] != ' ': play = int(input("Enter placement of next " + b[9] + ": "))
+    b[play], b[9], b[10], turn = b[9], b[10], b[9], turn + 1
+    print(' '+b[0]+' | '+b[1]+' | '+b[2]+' \n___|___|___\n '+b[3]+' | '+b[4]+' | '+b[5]+' \n___|___|___\n '+b[6]+' | '+b[7]+' | '+b[8]+ ' \n   |   |   ')
+print(b[10] + " wins!\n" if (((b[0]==b[4]==b[8] or b[1]==b[4]==b[7] or b[2]==b[4]==b[6] or b[3]==b[4]==b[5]) and b[4] in 'XO')or ((b[0]==b[1]==b[2] or b[0]==b[3]==b[6]) and b[0]in 'XO')or ((b[8]==b[7]==b[6] or b[5]==b[8]==b[2]) and b[8]in 'XO')) else "It's a tie! One of you needs to get better")
